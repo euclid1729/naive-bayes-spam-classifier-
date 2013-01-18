4912e15c48cd09347371c2b16a3a9f275e2347f1
Siddharth Sharma <siddharth.1729@gmail.com>
2013-01-18 17:39:14 -0500

    Update bayesnet.py

--- bayesnet.py
+++ bayesnet.py
@@ -3,13 +3,17 @@ import math,re
 class bayesean:
  def __init__(self):
  #dictionary to hold count of a word in a category e.g. {'word1':{'category1':count1,'category2':count2}}}
-  fcat_count={}
+  self.fcat_count={}
  #dictionary to hold count of number of documents in each category 
-  category={} 
+  self.category={} 
  
- def getwords(doc):
+ def getwords(self,doc):
   split_logic=re.compile('\\W*')
-  words=[s.lower() for s in split_logic.split(doc)]
+  content=''
+  for line in file(doc):
+   content+=line
+   content+=' '
+  words=[s.lower() for s in split_logic.split(content)]
   return dict([(word,1) for word in words])
  
  def increase_feature_count(self,feature,cat):
@@ -19,13 +23,13 @@ class bayesean:
  
  #increasing the count of category 
  def increaseCategoryCount(self,cat):
-  self.cc.setdefault(cat,0)
+  self.category.setdefault(cat,0)
   self.category[cat]+=1
  
  #return number of times a feature has occurred in a particular category 
  def getFeatureCount(self,feature,cat):
   if feature in self.fcat_count and cat in self.fcat_count[feature]:
-   return self.fcat_count[feature][cat]
+   return float(self.fcat_count[feature][cat])
  
  #return number of documents in a category
  def getCategoryCount(self,cat):
@@ -34,7 +38,7 @@ class bayesean:
  
  def getCountTotalDocument(self):
   return sum(self.category.values())
- 
+  
  def getListCategories(self):
   return self.category.keys()
  
@@ -42,20 +46,20 @@ class bayesean:
  def parseDoc(self,doc,cat):
   words=self.getwords(doc)
   for feature in words:
-   increase_feature_count(feature,cat)
+   self.increase_feature_count(feature,cat)
   #increase the category count 
-  increaseCategoryCount(cat)
+  self.increaseCategoryCount(cat)
  
  def getFeatureProb(self,f,cat):
-  if self.getCategoryCount(cat)==0:
-   return 0
-  return self.getFeatureCount(f,cat)/self.getCategoryCount(cat)
+  if self.getCategoryCount(cat) ==0: return 0
+  if self.getFeatureCount(f,cat) == None: return 0
+  else : return (self.getFeatureCount(f,cat)/self.getCategoryCount(cat))
  
  # "add one smoothing", for handling probabilites for categories or features that are seen first time in test data  
  
  def getWeightedProb(self,f,cat,weight=1.0,ap=0.5):
-  org_prob=getFeatureProb(f,cat) 
-  totals=sum([self.getFeatureCounts(f,c) for c in self.getListCategories( )])
+  org_prob=self.getFeatureProb(f,cat) 
+  totals=sum([self.getFeatureCount(f,c) for c in self.getListCategories( ) if self.getFeatureCount(f,c) is not None])
   bp=((weight*ap)+(totals*org_prob))/(weight+totals)
   return bp
  
@@ -70,29 +74,30 @@ class bayesean:
  
  #calculating probability(category|doc)
  def getCategoryDocProbability(self,doc,cat):
-  probDoc=self.getDoccategoryProbability(doc,cat)
+  probDoc=self.getDocCategoryProbabilty(doc,cat)
   probCat=self.getCategoryCount(cat)/self.getCountTotalDocument()  
   return probDoc*probCat
  
  #for spam classifcation, we are putting a threshold on the difference between prob of spam and non spams since we dont want to categorize non spam as spam just because the probability(spam|doc) is just a little more than probability(non spam|doc)
  
  def NaiveBayesClassify(self,doc,default='non spam'):
-  max_prob==o
+  max_prob=0
   probs={} # this dictionary will keep values of probabilites associated with each spam 
   #we are taking threshold vale as 2
-  threshold=2 
+  threshold=2
+  best=self.getListCategories()[0]
   for c in self.getListCategories():
-   probs[c]=self.getCategoryDocProbabilty(doc,c)
-   if probs[c] > max:
-    max=probs[c]
-    best=c 
+   probs[c]=self.getCategoryDocProbability(doc,c)
+   if probs[c] > max_prob:
+    max_prob=probs[c]
+    best=c
   for c in probs:
    if c == best: continue
-   if probs[c] * 2 > probs[best]: return default
+   if probs[c] * 2 > max_prob: return default
   return best
- 
+  
  def trainAll(self,path,cat):
   import os
   listing = os.listdir(path)
   for infile in listing:
-   parseDoc(infile,cat)  
+   self.parseDoc(path+'/'+infile,cat)  
